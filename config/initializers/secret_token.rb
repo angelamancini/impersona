752bd3ea4e9fc141c5cf8fda244318a771bb8a68
Angela Mancini <angela.mancini@sage.com>
2014-11-01 16:14:01 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Impersona::Application.config.secret_key_base = '6c97cc0a62959d3475932c720a872b677356b3b2b678f9e9cad41991008468fe047c7da4484af0abf56ba269984b5d081c2c21451cb7fd39d44233773abef531'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Impersona::Application.config.session_store :cookie_store, key: '_impersona_session'
